f83f65cece58471987c4b28372692929aaccaccf
Kirill Ragozin <ragozin@hey.com>
2023-08-15 04:01:11 +0400

    discart email form

--- JS/contact.js
+++ JS/contact.js
@@ -40,270 +40,297 @@ var textSuccessful = "Form submitted successfully!";
 
 // Click to Submit Button, Get Values of Input Fields, Validate
 function sendContact()
-{
+{   
+    console.log('clicked');
+    event.preventDefault();
 
-    // Style Error and Submit Message
-    errorMessage.style.padding = "10px";
-    submitMessage.style.padding = "10px";
+    // // Style Error and Submit Message
+    // errorMessage.style.padding = "10px";
+    // submitMessage.style.padding = "10px";
 
-    // Validate Name
-    if(nameVar.length < 5)
-    {
-        errorMessage.innerHTML = textName;
-        submitMessage.style.display = "none";
-        return false;
-    }
+    // // // Validate Name
+    // // if(nameVar.length < 1)
+    // // {
+    // //     errorMessage.innerHTML = textName;
+    // //     submitMessage.style.display = "none";
+    // //     return false;
+    // // }
 
-    // Validate Email
-    if(mail.indexOf("@") == -1 || mail.length < 6) {
-        errorMessage.innerHTML = textMail;
-        submitMessage.style.display = "none";
-        return false;
-    }
-
-    // Validate Address
-    // if(address.length < 5)
-    // {
-    //     errorMessage.innerHTML = textAddress;
+    // // Validate Email
+    // if(mail.indexOf("@") == -1 || mail.length < 6) {
+    //     errorMessage.innerHTML = textMail;
     //     submitMessage.style.display = "none";
     //     return false;
     // }
 
-    // // Validate Phone
-    // if(isNaN(phone) || phone.length != 13)
-    // {
-    //     errorMessage.innerHTML = textPhone;
-    //     submitMessage.style.display = "none";
-    //     return false;
-    // }
+    // // Validate Address
+    // // if(address.length < 5)
+    // // {
+    // //     errorMessage.innerHTML = textAddress;
+    // //     submitMessage.style.display = "none";
+    // //     return false;
+    // // }
 
-    // Validate Message
-    if(message.length <= 10) {
-        errorMessage.innerHTML = textMessage;
-        submitMessage.style.display = "none";
-        return false;
-    }
+    // // // Validate Phone
+    // // if(isNaN(phone) || phone.length != 13)
+    // // {
+    // //     errorMessage.innerHTML = textPhone;
+    // //     submitMessage.style.display = "none";
+    // //     return false;
+    // // }
 
-    // Show Submission Successful Message
-    errorMessage.style.display = "none";
-    submitMessage.style.display = "block";
-    submitMessage.innerHTML = textSuccessful;
+    // // // Validate Message
+    // // if(message.length <= 10) {
+    // //     errorMessage.innerHTML = textMessage;
+    // //     submitMessage.style.display = "none";
+    // //     return false;
+    // // }
+
+    // // Show Submission Successful Message
+    // errorMessage.style.display = "none";
+    // submitMessage.style.display = "block";
+    // submitMessage.innerHTML = textSuccessful;
 
     // Dissapear Info After 5 Seconds
-    setTimeout(function()
-    {
-        $(".submitted").remove();
-        $(".cForm").trigger("reset");
-    }, 5000
-    );
+    // setTimeout(function()
+    // {
+    //     $(".submitted").remove();
+    //     $(".cForm").trigger("reset");
+    // }, 5000
+    // );
+
+    var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
 
 
 }
 
 
-if (screenSize2400.matches) 
-{
-    function sendContact() 
-    {
-        errorMessage.style.padding = "12.5px";
-        submitMessage.style.padding = "12.5px";
-        // Validate Name
-        if(nameVar.length < 5)
-        {
-            errorMessage.innerHTML = textName;
-            submitMessage.style.display = "none";
-            return false;
-        }
+// if (screenSize2400.matches) 
+// {
+//     function sendContact() 
+//     {
+//         errorMessage.style.padding = "12.5px";
+//         submitMessage.style.padding = "12.5px";
+//         // // Validate Name
+//         // if(nameVar.length < 1)
+//         // {
+//         //     errorMessage.innerHTML = textName;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Email
-        if(mail.indexOf("@") == -1 || mail.length < 6) {
-            errorMessage.innerHTML = textMail;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // Validate Email
+//         if(mail.indexOf("@") == -1 || mail.length < 6) {
+//             errorMessage.innerHTML = textMail;
+//             submitMessage.style.display = "none";
+//             return false;
+//         }
 
-        // Validate Address
-        // if(address.length < 5)
-        // {
-        //     errorMessage.innerHTML = textAddress;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // Validate Address
+//         // if(address.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textAddress;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // // Validate Phone
-        // if(isNaN(phone) || phone.length != 13)
-        // {
-        //     errorMessage.innerHTML = textPhone;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // // Validate Phone
+//         // if(isNaN(phone) || phone.length != 13)
+//         // {
+//         //     errorMessage.innerHTML = textPhone;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Message
-        if(message.length <= 140) {
-            errorMessage.innerHTML = textMessage;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // // Validate Message
+//         // if(message.length <= 140) {
+//         //     errorMessage.innerHTML = textMessage;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Show Submission Successful Message
-        errorMessage.style.display = "none";
-        submitMessage.style.display = "block";
-        submitMessage.innerHTML = textSuccessful;
-    }
-}
-
-
-if (screenSize3000.matches) 
-{
-    function sendContact() 
-    {
-        errorMessage.style.padding = "15.6px";
-        submitMessage.style.padding = "15.6px";
-        // Validate Name
-        if(nameVar.length < 5)
-        {
-            errorMessage.innerHTML = textName;
-            submitMessage.style.display = "none";
-            return false;
-        }
-
-        // Validate Email
-        if(mail.indexOf("@") == -1 || mail.length < 6) {
-            errorMessage.innerHTML = textMail;
-            submitMessage.style.display = "none";
-            return false;
-        }
-
-        // Validate Address
-        // if(address.length < 5)
-        // {
-        //     errorMessage.innerHTML = textAddress;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
-
-        // // Validate Phone
-        // if(isNaN(phone) || phone.length != 13)
-        // {
-        //     errorMessage.innerHTML = textPhone;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
-
-        // Validate Message
-        if(message.length <= 140) {
-            errorMessage.innerHTML = textMessage;
-            submitMessage.style.display = "none";
-            return false;
-        }
-
-        // Show Submission Successful Message
-        errorMessage.style.display = "none";
-        submitMessage.style.display = "block";
-        submitMessage.innerHTML = textSuccessful;
-    }
-}
+//         // Show Submission Successful Message
+//         errorMessage.style.display = "none";
+//         submitMessage.style.display = "block";
+//         submitMessage.innerHTML = textSuccessful;
+//     }
+// }
 
 
-if (screenSize3750.matches) 
-{
-    function sendContact() 
-    {
-        errorMessage.style.padding = "19.5px";
-        submitMessage.style.padding = "19.5px";
-        // Validate Name
-        if(nameVar.length < 5)
-        {
-            errorMessage.innerHTML = textName;
-            submitMessage.style.display = "none";
-            return false;
-        }
+// if (screenSize3000.matches) 
+// {
+//     function sendContact() 
+//     {
+//         errorMessage.style.padding = "15.6px";
+//         submitMessage.style.padding = "15.6px";
+//         // // Validate Name
+//         // if(nameVar.length < 1)
+//         // {
+//         //     errorMessage.innerHTML = textName;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Email
-        if(mail.indexOf("@") == -1 || mail.length < 6) {
-            errorMessage.innerHTML = textMail;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // Validate Email
+//         if(mail.indexOf("@") == -1 || mail.length < 6) {
+//             errorMessage.innerHTML = textMail;
+//             submitMessage.style.display = "none";
+//             return false;
+//         }
 
-        // Validate Address
-        // if(address.length < 5)
-        // {
-        //     errorMessage.innerHTML = textAddress;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // Validate Address
+//         // if(address.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textAddress;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // // Validate Phone
-        // if(isNaN(phone) || phone.length != 13)
-        // {
-        //     errorMessage.innerHTML = textPhone;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // // Validate Phone
+//         // if(isNaN(phone) || phone.length != 13)
+//         // {
+//         //     errorMessage.innerHTML = textPhone;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Message
-        if(message.length <= 140) {
-            errorMessage.innerHTML = textMessage;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // Validate Message
+//         if(message.length <= 140) {
+//             errorMessage.innerHTML = textMessage;
+//             submitMessage.style.display = "none";
+//             return false;
+//         }
 
-        // Show Submission Successful Message
-        errorMessage.style.display = "none";
-        submitMessage.style.display = "block";
-        submitMessage.innerHTML = textSuccessful;
-    }
-}
+//         // Show Submission Successful Message
+//         errorMessage.style.display = "none";
+//         submitMessage.style.display = "block";
+//         submitMessage.innerHTML = textSuccessful;
+//     }
+// }
 
 
-if (screenSizeLarge.matches) 
-{
-    function sendContact() 
-    {
-        errorMessage.style.padding = "24.3px";
-        submitMessage.style.padding = "24.3px";
-        // Validate Name
-        if(nameVar.length < 5)
-        {
-            errorMessage.innerHTML = textName;
-            submitMessage.style.display = "none";
-            return false;
-        }
+// if (screenSize3750.matches) 
+// {
+//     function sendContact() 
+//     {
+//         errorMessage.style.padding = "19.5px";
+//         submitMessage.style.padding = "19.5px";
+//         // // Validate Name
+//         // if(nameVar.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textName;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Email
-        if(mail.indexOf("@") == -1 || mail.length < 6) {
-            errorMessage.innerHTML = textMail;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // Validate Email
+//         if(mail.indexOf("@") == -1 || mail.length < 6) {
+//             errorMessage.innerHTML = textMail;
+//             submitMessage.style.display = "none";
+//             return false;
+//         }
 
-        // Validate Address
-        // if(address.length < 5)
-        // {
-        //     errorMessage.innerHTML = textAddress;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // Validate Address
+//         // if(address.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textAddress;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // // Validate Phone
-        // if(isNaN(phone) || phone.length != 13)
-        // {
-        //     errorMessage.innerHTML = textPhone;
-        //     submitMessage.style.display = "none";
-        //     return false;
-        // }
+//         // // Validate Phone
+//         // if(isNaN(phone) || phone.length != 13)
+//         // {
+//         //     errorMessage.innerHTML = textPhone;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Validate Message
-        if(message.length <= 140) {
-            errorMessage.innerHTML = textMessage;
-            submitMessage.style.display = "none";
-            return false;
-        }
+//         // Validate Message
+//         // if(message.length <= 140) {
+//         //     errorMessage.innerHTML = textMessage;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
 
-        // Show Submission Successful Message
-        errorMessage.style.display = "none";
-        submitMessage.style.display = "block";
-        submitMessage.innerHTML = textSuccessful;
-    }
-}
+//         // Show Submission Successful Message
+//         errorMessage.style.display = "none";
+//         submitMessage.style.display = "block";
+//         submitMessage.innerHTML = textSuccessful;
+//     }
+// }
+
+
+// if (screenSizeLarge.matches) 
+// {
+//     function sendContact() 
+//     {
+//         errorMessage.style.padding = "24.3px";
+//         submitMessage.style.padding = "24.3px";
+//         // Validate Name
+//         // if(nameVar.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textName;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
+
+//         // Validate Email
+//         if(mail.indexOf("@") == -1 || mail.length < 6) {
+//             errorMessage.innerHTML = textMail;
+//             submitMessage.style.display = "none";
+//             return false;
+//         }
+
+//         // Validate Address
+//         // if(address.length < 5)
+//         // {
+//         //     errorMessage.innerHTML = textAddress;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
+
+//         // // Validate Phone
+//         // if(isNaN(phone) || phone.length != 13)
+//         // {
+//         //     errorMessage.innerHTML = textPhone;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
+
+//         // Validate Message
+//         // if(message.length <= 140) {
+//         //     errorMessage.innerHTML = textMessage;
+//         //     submitMessage.style.display = "none";
+//         //     return false;
+//         // }
+
+//         // Show Submission Successful Message
+//         errorMessage.style.display = "none";
+//         submitMessage.style.display = "block";
+//         submitMessage.innerHTML = textSuccessful;
+//     }
+// }
